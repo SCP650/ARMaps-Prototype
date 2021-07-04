b1d28078c3238f25fad0cd3c889dc4eda681715a
Sebastian Yang <47506432+SCP650@users.noreply.github.com>
2021-07-04 14:21:12 -0400

    move along car working

---
+++ Assets/_Scripts/Managers/CameraManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] Transform PlayerHead;
+    [SerializeField] Transform BlueCarObject;
+
+   public void SwitchCameraView()
+    {
+        PlayerHead.SetParent(BlueCarObject);
+    }
+}
