b51467a0dece2bd128404487c5516bd8bbdb06f9
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2021-08-10 23:24:48 -0400

    fixed controller offset/

--- Assets/_Scripts/Managers/CityManager.cs
+++ Assets/_Scripts/Managers/CityManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CityManager : MonoBehaviour
 {
     public GameObject[] Cars;
+
+    public Vector3 CameraOffset = new Vector3(0, -0.4f, 0);
+    public Vector3 CameraRotation = new Vector3(0, 0, 0);
     private void Start()
     {
         UpdatePlayerLocation();
@@ -17,7 +20,9 @@ public class CityManager : MonoBehaviour
         if (player)
         {
             player.transform.parent = Cars[GameManager.Instance.CarNum].transform;
-            player.transform.localPosition = new Vector3(0,-0.4f,0);
+            //player.transform.localPosition = new Vector3(0,-0.4f,0);
+            player.transform.localPosition = CameraOffset;
+            player.transform.localRotation = Quaternion.Euler(CameraRotation);
         }
     }
 }
